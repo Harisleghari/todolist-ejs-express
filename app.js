1d4c5476a80de6d220d648aab8654e0c392c295e
Haris Leghari <harisleghari61@gmail.com>
2022-11-28 20:01:00 +0500

    EJS Baesd Todolist

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const newItems = [];
+
+app.get("/", function (req, res) {
+  const today = new Date();
+  const optoins = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  const day = today.toLocaleDateString('en-US', optoins);
+
+res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post('/', function(req, res){
+    const item = req.body.listinput;
+
+    if (req.body.button === "Work"){
+        newItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+    
+});
+
+
+app.get("/work", function(req, res){
+    res.render("list", { listTitle: "Work", newListItems: newItems });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
